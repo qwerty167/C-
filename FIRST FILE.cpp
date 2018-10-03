d954944909fc92d38d4624cc2cd0b2e38269b88b
qwerty167 <43601444+qwerty167@users.noreply.github.com>
2018-10-03 11:07:04 +0300

    Качен файл 123
    Кифте

---
+++ FIRST FILE.cpp
@@ -0,0 +1,8 @@
+#include<iostream>
+using namespace std;
+int main ()
+
+{
+//MOQT NAI SKYCHEN CHAS//
+cout<<"skykaaaa"<<end;
+return 0;
---
+++ proba.cpp
@@ -0,0 +1,8 @@
+#include<iostream>
+using namespace std;
+int main ()
+
+{
+cout<<"kariiii O<===  xxx <3";
+return 0;
+}
